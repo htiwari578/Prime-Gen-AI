748ef3dd70064c52bb6bf0ba75a2ab03142d893d
Himanshu Tiwari <htiwari578@gmail.com>
2024-11-28 23:05:32 +0530

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-import { genContent } from "@/lib/action";
-import Image from "next/image";
+
 
 export default async function Home() {
 
-  const data = await genContent("hello");
-  return <div>{data.content}</div>
+ 
+  return <div>Home</div>
 }
