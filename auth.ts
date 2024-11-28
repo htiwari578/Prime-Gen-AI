a715b9e52152543c46c01733555946f664bfde97
Himanshu Tiwari <htiwari578@gmail.com>
2024-11-28 21:35:05 +0530

    pisma

--- auth.ts
+++ auth.ts
@@ -1,5 +1,28 @@
 import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+import { NextResponse } from "next/server";
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "./prisma/prisma";
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  adapter:PrismaAdapter(prisma),
+  providers: [GitHub],
+
+  session: {
+    strategy: "jwt",
+  },
+
+  callbacks:{
+    authorized({auth,request:{nextUrl}}){
+        const isLoggedIn = !!auth?.user
+        const isOnDashboard = nextUrl.pathname.includes("/dashboard");
+        if(isOnDashboard) {
+            if(isLoggedIn) return true
+            return false
+        }else if(!isLoggedIn && isOnDashboard){
+            return NextResponse.redirect(new URL("/api/auth/signin", nextUrl));
+        }
+        return true;
+    }
+  }
 })
